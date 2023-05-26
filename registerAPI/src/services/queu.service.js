16869d7ab042abb55c04591384af40c107ac0776
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-05-26 13:47:40 -0300

    fix: mail request

--- registerAPI/src/services/queu.service.js
+++ registerAPI/src/services/queu.service.js
@@ -8,10 +8,10 @@ const emailQueue = new Queue('emailNotification', {
   }
 });
 
-emailQueue.process(async (mailData) => {
+emailQueue.process(async (job) => {
   try {
-    console.log(`New data to process arrived: ${JSON.stringify(mailData)}`);
-    const result = await sendMail(mailData);
+    console.log(`New data to process arrived: ${JSON.stringify(job.data)}`);
+    const result = await sendMail(job.data);
     console.info(`Request to send an email to ${mail.email} response: `, result);
   } catch(err) {
     console.error(`Error while sending email to ${mail.email}...\nError: ${err.message}`);
--- registerAPI/src/services/sendMail.service.js
+++ registerAPI/src/services/sendMail.service.js
@@ -1,9 +1,13 @@
 const axios = require('axios');
 
 const sendMail = async ({ fullName, email, message }) => {
-  console.log(`Sending email through microservice...`);
-  const result = await axios.post('http://mailer_api:3000/send-mail', { fullName, email, message });
-  return result.data;
+  console.log(`Sending email through microservice to ${email}...`);
+  try {
+    const result = await axios.post('http://mailer_api:3000/send-mail', { fullName, email, message });
+    return result.data;
+  } catch(err) {
+    console.error(err.message);
+  }
 }
 
 module.exports = {
